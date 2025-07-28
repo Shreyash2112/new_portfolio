40cf6316939abdd721c2068890c09d235e5e80b6
Shreyash Purankar <purankar.shreyash54321@gmail.com>
2025-07-28 19:47:21 +0530

    Add emailjs functionality

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,11 +1,67 @@
 import { MdOutlineEmail, MdOutlineLocationOn, MdOutlinePhoneIphone } from "react-icons/md";
 import cn from "../../lib/utils";
+import emailjs from '@emailjs/browser';
+import { emailjsConfig } from "../../config/emailjs";
+import { useRef, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import { LuSend } from "react-icons/lu";
+
 function Contact() {
+    const { EMAILJS_PUBLIC_API_KEY, EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID } = emailjsConfig
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        message: "",
+    })
+
+    const [isSubmitting, setIsSubmitting] = useState(false)
+
+    const form = useRef()
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        setIsSubmitting(true)
+        try {
+            emailjs.sendForm(EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, form.current, { publicKey: EMAILJS_PUBLIC_API_KEY })
+                .then(() => {
+                    setFormData({ name: "", email: "", message: "" })
+                    toast.success('Message sent successfully 🚀', {
+                        position: "top-center",
+                        autoClose: 3000,
+                        hideProgressBar: false,
+                        closeOnClick: false,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "dark",
+                    });
+                })
+                .catch(() => {
+                    toast.error('Something went wrong! ❌', {
+                        position: "top-center",
+                        autoClose: 3000,
+                        hideProgressBar: false,
+                        closeOnClick: false,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "dark",
+                    });
+                })
+                .finally(() => {
+                    setIsSubmitting(false)
+                })
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
     return (
         <section
             id="contact"
             className="py-24 px-4 relative bg-secondary/30"
         >
+            <ToastContainer />
             <div className="container mx-auto max-w-5xl">
                 <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
                     Get In <span className="text-primary">Touch</span>
@@ -64,13 +120,15 @@ function Contact() {
 
                     <div className="bg-card p-8 rounded-lg shadow-xs">
                         <h3 className="text-2xl font-semibold mb-6">Send a Message</h3>
-                        <form className="space-y-6">
+                        <form
+                            ref={form}
+                            onSubmit={handleSubmit}
+                            className="space-y-6">
                             <div className="text-left">
                                 <label
                                     htmlFor="name"
                                     className="block text-sm font-medium mb-2 pl-1"
                                 >
-                                    {" "}
                                     Name
                                 </label>
                                 <input
@@ -80,6 +138,8 @@ function Contact() {
                                     required
                                     className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary"
                                     placeholder="username"
+                                    value={formData.name}
+                                    onChange={(e) => setFormData({ ...formData, name: e.target.value })}
                                 />
                             </div>
 
@@ -97,6 +157,8 @@ function Contact() {
                                     required
                                     className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary"
                                     placeholder="user@gmail.com"
+                                    value={formData.email}
+                                    onChange={(e) => setFormData({ ...formData, email: e.target.value })}
                                 />
                             </div>
 
@@ -113,19 +175,20 @@ function Contact() {
                                     required
                                     className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary resize-none"
                                     placeholder="Hello, I'd like to talk about..."
+                                    value={formData.message}
+                                    onChange={(e) => setFormData({ ...formData, message: e.target.value })}
                                 />
                             </div>
 
                             <button
                                 type="submit"
-                                // disabled={isSubmitting}
+                                disabled={isSubmitting}
                                 className={cn(
                                     "cosmic-button w-full flex items-center justify-center gap-2"
                                 )}
                             >
-                                Send Message
-                                {/* {isSubmitting ? "Sending..." : "Send Message"} */}
-                                {/* <Send size={16} /> */}
+                                {isSubmitting ? "Sending..." : "Send Message"}
+                                <LuSend size={18} />
                             </button>
                         </form>
                     </div>
---
+++ src/config/emailjs.js
@@ -0,0 +1,5 @@
+export const emailjsConfig = {
+  EMAILJS_PUBLIC_API_KEY: String(import.meta.env.VITE_EMAILJS_PUBLIC_API_KEY),
+  EMAILJS_SERVICE_ID: String(import.meta.env.VITE_EMAILJS_SERVICE_ID),
+  EMAILJS_TEMPLATE_ID: String(import.meta.env.VITE_EMAILJS_TEMPLATE_ID),
+};
